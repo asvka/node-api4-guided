e701ba900eaa326390cacab7948408028ae805a1
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-04-15 17:51:39 -0700

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // this reads in all of our environment variables from .env
-// require("dotenv").config()
+require("dotenv").config()
 
 const express = require("express")
 const helmet = require("helmet")
